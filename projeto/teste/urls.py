ee33ae5515e5ff7f7e83f2d8e545e1556c87db63
IanPT100 <148597944+IanPT100@users.noreply.github.com>
2024-01-23 10:52:31 -0300

    Add files via upload

---
+++ projeto/teste/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns =[
+    path('', views.index, name='index'),
+
+    path('listar_aluno', views.listarAluno, name='listar_aluno'),
+    path('listar_curso', views.listarCurso, name='listar_curso'),
+    path('incluir_aluno', views.incluirAluno, name='incluir_aluno'),
+    path('editar_aluno/<int:id>', views.editarAluno, name='editar_aluno'),
+    path('incluir_cursos/', views.incluir_cursos, name='incluir_cursos'),
+    path('editar_curso/<int:id>', views.editarCurso, name='editar_curso'),
+    path('excluir_aluno/<int:id>', views.excluirAluno,name='excluir_aluno'),
+    path('excluir_curso/<int:id>', views.excluirCurso,name='excluir_curso'),
+]
